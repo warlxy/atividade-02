ab9c246e4182dde5d766603846faee00455238dc
aluno <>
2024-02-29 20:28:18 -0300

    feat: integração do CadastroScreen, ListaContato e LoginScreen

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from "./src/screens/LoginScreen"
+import CadastroUsuario from "./src/screens/CadastroScreen"
+import ListaContato from "./src/screens/ListaContato"
+import { TouchableOpacity } from 'react-native-web';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+
+        <Stack.Screen name="Login" component={Login} options={{ 
+          headerStyle: {
+            backgroundColor: '#1670f7',
+          },
+          headerTintColor: '#fff',
+          headerTitleAlign: 'center',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            fontSize:20,
+          } }}/>
+
+        <Stack.Screen name="Usuário" component={CadastroUsuario} options={{ 
+          headerStyle: {
+            backgroundColor: '#1670f7',
+          },
+          headerTintColor: '#fff',
+          headerTitleAlign: 'center',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            fontSize:20,
+          } }}/>
+
+        <Stack.Screen name="Lista de Contato" component={ListaContato} options={{ 
+          headerLeft: null,
+          headerStyle: {
+            backgroundColor: '#1670f7',
+          },
+          headerTintColor: '#fff',
+          headerTitleAlign: 'center',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            fontSize:20,
+          },
+          // headerRight: () => (
+          //   <TouchableOpacity onPress={() => Navigation.navigate('Contato')}>
+          //     <
+          //   </TouchableOpacity>
+          // )
+           }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#000',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   teste: {
+//     backgroundColor: '#000',
+//     textAlign: 'center'
+//   }
+// });
